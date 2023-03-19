794a94af01a2966e350e47ff93ee42494ea47717
Andreea Mateescu <andreea.mateescu@source.ag>
2023-03-19 22:06:58 +0100

    EDA

--- reading_validation_datasets.py
+++ reading_validation_datasets.py
@@ -4,31 +4,20 @@ import os
 
 validation_ds_path = Path(__file__).parent / 'validation_datasets'
 
-for subfolder in os.listdir(validation_ds_path):
-    if os.path.isdir(os.path.join(validation_ds_path, subfolder)):
-        csv_path = os.path.join(validation_ds_path, subfolder, subfolder + '.csv')
-        country = pd.read_csv(csv_path, sep=',')
-        subset_land_cover = country[
+
+def read_all_validation_files(validation_folder_path):
+    """This function reads all validation datasets, and saves the validation datasets concerning
+    latitutde, longitude, land cover, land use information for woodland ares"""
+    for subfolder in os.listdir(validation_folder_path):
+        if os.path.isdir(os.path.join(validation_ds_path, subfolder)):
+            csv_path = os.path.join(validation_ds_path, subfolder, subfolder + '.csv')
+            country = pd.read_csv(csv_path, sep=',')
+            subset_land_cover = country[
                 ['POINT_ID', 'TH_LAT', 'TH_LONG', 'LC1', 'LC2', 'LU1', 'TREE_HEIGHT_SURVEY', 'TREE_HEIGHT_MATURITY']]
-        subset_land_cover = subset_land_cover.loc[
-            subset_land_cover['LC1'].isin(['C10', 'C21', 'C22', 'C23', 'C31', 'C32', 'C33'])]
+            subset_land_cover = subset_land_cover.loc[
+                subset_land_cover['LC1'].isin(['C10', 'C21', 'C22', 'C23', 'C31', 'C32', 'C33'])]
 
-        new_csv_path = os.path.join(validation_ds_path, subfolder, 'new_' + subfolder + '.csv')
-        subset_land_cover.to_csv(new_csv_path, index=False)
-
-# 2 make statistics of those points including
-
-# 3 make some checks, using plotly and the latitude and longitude, of how extended this dataset is
-
-# * download and compare 2018 data for other 2 countries that are nearby France and / or seem to share the same type of forest
-
-# * check in LUCAS db what other interesting data points you can get
-
-# 4 check well to what other countries the study can be generalised
-
-# 6 make some notes related to potential downsides of the evaluation data
-
-# 4 check with the dates as well
+            new_csv_path = os.path.join(validation_ds_path, subfolder, 'new_' + subfolder + '.csv')
+            subset_land_cover.to_csv(new_csv_path, index=False)
 
 
-# print(value_counts_dict)
